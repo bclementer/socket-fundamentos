1e47d476c04df5c05934df55d9128dafdb639fd9
Beatriz <beaclemen90@gmail.com>
2020-06-16 15:46:20 +0200

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,24 @@
+var socket = io();
+
+// Escuchar
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+// Escuchar
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor.');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Beatriz',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
